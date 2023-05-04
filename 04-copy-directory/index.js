2f1679e0ce57bad3e3dc0f0ca52da78bcfcf438b
Andrei <ai.dr@yandex.ru>
2023-05-04 15:53:32 +0400

    feat: complete 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,55 @@
+const fs = require('fs');
+const path = require('path');
 
+/* const copy =  */
+function copyDir(){
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+    if (err) throw err;
+    });
+    
+    fs.promises.readdir((path.join(__dirname, 'files-copy'))).then(arr=>{
+        arr.forEach( el=>{
+            console.log(el)
+                fs.unlink(path.join(__dirname, 'files-copy', el), err =>{
+                    if(err) throw err; 
+                })
+        })
+    })
+
+
+    fs.promises.readdir((path.join(__dirname, 'files'))).then(arr=>{
+            arr.forEach( el=>{
+                fs.promises.copyFile(path.join(__dirname, 'files', el),path.join(__dirname, 'files-copy', el))})
+        })
+}
+copyDir();
+
+/* fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+    if (err) throw err;
+  
+    for (const file of files) {
+        console.log(files)
+      fs.unlink(path.join(__dirname, 'files-copy', file), err => {});
+    }
+  }); 
+ */
+/*       fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+        if (err) throw err;
+        for (const file of files) {
+            if(files.length == 0){
+                return;
+            } else{
+                console.log(files)
+                fs.unlink(path.join(__dirname, 'files-copy', file), err => {});
+            }
+
+        }
+      });  */
+
+      /*     fs.promises.readdir((path.join(__dirname, 'files-copy'))).then(arr=>{
+        arr.forEach( el=>{
+                fs.unlink(path.join(__dirname, 'files-copy', el), err =>{
+                    if(err) throw err; 
+                })
+        })
+    }) */

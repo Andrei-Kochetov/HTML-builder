0aaa0b90b3e4a1629505053bd69117bf6f0e5a27
Andrei <ai.dr@yandex.ru>
2023-05-08 18:16:20 +0400

    refactor: clean files for testing

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,12 +5,10 @@ const output = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle
 
 stylesDir.then(arr=>{
     arr.forEach( (el)=>{
-        //console.log(arr)
         if(el.isFile() && el.name.split('.')[1] === 'css'){
-            //console.log(el)
-            const input = fs.createReadStream(path.join(__dirname, 'styles', el.name)/* , {highWaterMark: 1024} */);
-            input.on('data', chunk => output.write(chunk/* .toString() */ /* + '\n' */));
-            //input.pipe(output);
+            const input = fs.createReadStream(path.join(__dirname, 'styles', el.name));
+            input.on('data', chunk => output.write(chunk));
+
         }
     })
 })
